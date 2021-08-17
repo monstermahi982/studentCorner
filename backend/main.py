852498595f1c0d6f2b9bed77dc8deb42b46d2542
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-08-17 19:19:59 +0530

    working in database file

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,5 +11,15 @@ def read_root():
 
 
 @app.get("/about/{id}")
-def read_root(id):
-    return {"Hello": f"this is number {id}"}
+def read_root(id, name):
+    return {"Hello": f"this is number {id} and name is {name}"}
+
+
+class Data(BaseModel):
+    name: str
+    age: int
+
+
+@app.post("/")
+def add(request: Data):
+    return {"data": f"name is {request.name} and age is {request.age}"}
