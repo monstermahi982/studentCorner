050cd02157ed55a9470c5c5bee712d227dc52734
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-08-18 20:43:16 +0530

    started api

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,62 @@
+from bson.objectid import ObjectId
+from pymongo import MongoClient
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+import datetime
 
 app = FastAPI()
+client = MongoClient('localhost', 27017)
+db = client['studentCorner']
+collection = db['student']
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/about/{id}")
-def read_root(id, name):
-    return {"Hello": f"this is number {id} and name is {name}"}
-
-
-class Data(BaseModel):
+class Student(BaseModel):
     name: str
+    email: str
     age: int
+    year: int
+    password: str
+    date: str
 
 
-@app.post("/")
-def add(request: Data):
-    return {"data": f"name is {request.name} and age is {request.age}"}
+class UpdateStudent(BaseModel):
+    name: Optional[str]
+    email: Optional[str]
+    age: Optional[int]
+    year: Optional[int]
+    password: Optional[str]
+    date: str
+
+
+def student_helper(student) -> dict:
+    return {
+        "id": str(student["_id"]),
+        "name": student["name"],
+        "age": student["age"],
+        "gender": student["gender"],
+        "rollno": student["rollno"],
+    }
+
+
+@app.get("/student")
+def fetch_all_student():
+    Studentdata = []
+    for data in collection.find():
+        Studentdata.append(student_helper(data))
+    return {"date": data}
+
+
+@app.get("/student/{id}")
+def fetch_one_student(id):
+    return {"Hello": f"this is number {id}"}
+
+
+@app.post('/student')
+async def addStudent(request: Student):
+    return {"data": request}
+
+
+@app.put('/student/{id}')
+def UpdateStudent(request: UpdateStudent, id):
+    return {"data": request}
