1f536cce8a2d09a790087c687b8aa5bfe5199c85
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-08-21 19:55:43 +0530

    added file upload features

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,26 @@
+import os
 from bson.objectid import ObjectId
 from pymongo import MongoClient
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
 import datetime
-# from routes.user import user
 from schemas.user import userEntity, usersEntity
+from fastapi.staticfiles import StaticFiles
 
+# initialize
 app = FastAPI()
 
-# app.include_router(user)
+# file uploads
+app.mount("/static", StaticFiles(directory="static"), name="static")
+path = "/home/mahesh/projects/fastapi/app1/backend/static"
 
+# database connection
 client = MongoClient('localhost', 27017)
 db = client['studentCorner']
 collection = db['student']
+
+# class models
 
 
 class Student(BaseModel):
@@ -30,6 +37,7 @@ class UpdateStudent(BaseModel):
     gender: Optional[str]
 
 
+# routes
 @app.get("/student")
 def fetch_all_student():
     return usersEntity(collection.find())
@@ -59,3 +67,13 @@ def student_delete(id):
     collection.find_one_and_delete(
         {"_id": ObjectId(id)})
     return {"data": "deleted"}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    filePath = os.path.join(path, file.filename)
+    with open(filePath, 'wb+') as f:
+        f.write(contents)
+        f.close()
+    return {"filename": "static/"+file.filename}
